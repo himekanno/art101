522bbef08ba7c35b7ecd3be8a6d4e3b1c5b9c483
himekanno <hjkanno@ucsc.edu>
2021-01-25 21:40:48 -0800

    .

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,1 +1,21 @@
-//
+//Display categories
+  //Accept user input
+    //Generate random word from category
+//Print board for each round
+  //Print letters
+    //Print border around each letter
+  //Print space for number of letters in generated word
+  //Print "lives" counter
+    //start with ten lives
+//Accept user input
+  //Compare input to answer
+  //Check for match
+    //If “yes”, display chosen letter in correct space
+      //Check for win condition
+        //If “yes”, display “You Win!”
+          //Restart round
+        //If “no” continue to accept input
+    //If “no”, take one life
+      //Display lives number minus one
+        //If lives counter reaches zero, display “You Lose”
+          //Restart round
