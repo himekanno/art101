e3bead7051c1390bc6a0e184285620c12c88321d
himekanno <hjkanno@ucsc.edu>
2021-01-27 20:47:04 -0800

    lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Hime Kanno
+ * Created: 27 January 2021
+ */
+
+// Define Variables
+var make = "Toyota";
+var model = "C-HR";
+var color = "Black";
+var year = 2019;
+var ownIt = false;
+
+// Calculate Age
+age = 2021 - year;
+
+// Output Variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
