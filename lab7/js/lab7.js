4cf522e217fef9aecca3f3e3ed475df954de16c6
himekanno <hjkanno@ucsc.edu>
2021-02-03 19:48:49 -0800

    lab 7

---
+++ lab7/js/lab7.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Hime Kanno
+ * Created: 30 January 2021
+ */
+
+// sortUserName
+ function sortUserName(userName) {
+     console.log("userName = ", userName);
+     // split name into array
+     var nameArray = userName.split("");
+     console.log("nameArray = ", nameArray);
+     // sort array
+     var nameArraySort = nameArray.sort();
+     console.log("nameArraySort = ", nameArraySort);
+     // join array to make new name
+     var nameSorted = nameArraySort.join("");
+     console.log("nameSorted = ", nameSorted);
+     return nameSorted;
+ }
+
+// output new name
+ document.writeln("Here's your new name made just for you: ",
+     sortUserName(window.prompt("Please enter your name so I can fix it!")), "</br>");
