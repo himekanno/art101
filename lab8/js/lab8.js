5fd9bfbb3508868daffa69334a7b174ca70c4a45
himekanno <hjkanno@ucsc.edu>
2021-02-09 23:05:15 -0800

    .

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -3,57 +3,56 @@
  * Created: 30 January 2021
  */
 
- //Function 1 for first thing
- function firstThing(test){
-   console.log(test + ": This is the first thing.<br>")
+//Function 1 for first thing
+  function firstThing(test){
+    console.log(test + ": This is the first thing.<br>")
 
- }
+  }
 
- //firstFunction 2 for first thing
- function secondtThing(test){
-   console.log(test + ": This is the second thing.<br>")
+//firstFunction 2 for first thing
+  function secondtThing(test){
+    console.log(test + ": This is the second thing.<br>")
 
- }
+  }
 
- //Function 3 for first thing
- function thirdThing(test){
-   console.log(test + ": This is the third thing.<br>")
+//Function 3 for first thing
+  function thirdThing(test){
+    console.log(test + ": This is the third thing.<br>")
 
- }
+  }
 
+//First set of tests
+  firstThing("TEST 1");
+  secondtThing("TEST 1");
+  thirdThing("TEST 1");
 
- //First set of tests
- firstThing("TEST 1");
- secondtThing("TEST 1");
- thirdThing("TEST 1");
+//Second set of tests
+  setTimeout(function() {
+    firstThing("TEST 2");
+  }, 0);
 
- //Second set of tests
- setTimeout(function() {
-   firstThing("TEST 2");
- }, 0);
+  setTimeout(function() {
+    secondtThing("TEST 2");
+  }, 0);
 
- setTimeout(function() {
-   secondtThing("TEST 2");
- }, 0);
+  setTimeout(function() {
+    thirdThing("TEST 2");
+  }, 0);
 
- setTimeout(function() {
-   thirdThing("TEST 2");
- }, 0);
+// Third set of tests
+  setTimeout(function() {
+    firstThing("TEST 3");
+  }, 3000);
+  setTimeout(function() {
+    secondtThing("TEST 3");
+  }, 1000);
+  setTimeout(function() {
+    thirdThing("TEST 3");
+  }, 2000);
 
- // Third set of tests
- setTimeout(function() {
-   firstThing("TEST 3");
- }, 3000);
- setTimeout(function() {
-   secondtThing("TEST 3");
- }, 1000);
- setTimeout(function() {
-   thirdThing("TEST 3");
- }, 2000);
-
- //Bonus Task
- var numbers = [1, 2, 3, 4, 5, , 7, 8, 9]
- var oddnumbers = numbers.filter(function(number){
-   return number % 2;
- });
- console.log(oddnumbers);
+//Bonus Task
+  var numbers = [1, 2, 3, 4, 5, , 7, 8, 9]
+  var oddnumbers = numbers.filter(function(number){
+    return number % 2;
+  });
+  console.log(oddnumbers);
