7a84e14aad191884c2c4dcebcdb923c6aab772c8
himekanno <hjkanno@ucsc.edu>
2021-02-10 19:53:34 -0800

    lab9

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -1,16 +1,33 @@
 /*
  * Author: Hime Kanno
- * Created: 30 January 2021
+ * Created: 9 February 2021
  */
 
-function firstThing(test) {
-  console.log("test:",test)
-}
+var OutputEl = document.getElementById("output");
 
-function secondThing(test) {
-  console.log(test);
-}
+var new1El = document.createElement("p");
+new1El.innerHTML = "This is something new!";
+new1El.style.color = "DimGray";
+new1El.style.font = "italic bold 20px arial,serif";
 
-function firstThing(test) {
-  console.log(test);
+var new2El = document.createElement("p")
+new2El.innerHTML = "This is something newer!";
+new2El.style.color = "DarkSlateGray";
+new2El.style.font = "italic bold 30px arial,serif";
+
+OutputEl.appendChild(new1El);
+
+OutputEl.appendChild(new2El);
+
+var divEl = document.getElementById("div_1");
+divEl.innerHTML = "Click the button?";
+console.log(divEl.innerHTML);
+
+var button = document.createElement("button");
+document.body.appendChild(button);
+button.innerHTML = "Press me!";
+
+button.onclick = function(){
+  divEl.innerHTML= "Thanks for clicking!"
+
 }
